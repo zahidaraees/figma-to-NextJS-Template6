1e1834a4ade9b23a72827c7c25554fe1e854a3ce
Zahida Raees <160402259+zahidaraees@users.noreply.github.com>
2024-12-10 09:20:17 +0500

    Update
    Updated hoover color within links

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -11,19 +11,19 @@ export default function Footer() {
         <div className="space-y-3">
           <h6 className="text-sm text-black font-medium">Funiro</h6>
           <ul className="space-y-1">
-            <li className="text-[13px] text-gray-400">400 University Drive Suite 200 Caral</li>
-            <li className="text-[13px] text-gray-400 hover:text-white">Gables</li>
-            <li className="text-[13px] text-gray-400 hover:text-white">FL 33134 USA</li>
+            <li className="text-[13px] text-gray-400 hover:text-blue-800 ">400 University Drive Suite 200 Caral</li>
+            <li className="text-[13px] text-gray-400 hover:text-blue-800">Gables</li>
+            <li className="text-[13px] text-gray-400 hover:text-blue-800">FL 33134 USA</li>
           </ul>
         </div>
 
         <div className="space-y-3">
-          <h6 className="text-sm text-gray-400 font-medium">LINKS</h6>
+          <h6 className="text-sm text-gray-800 font-medium">LINKS</h6>
           <ul className="space-y-2">
-            <li><a href="javascript:void(0)" className="text-[13px] text-black hover:text-white">Home</a></li>
-            <li><a href="javascript:void(0)" className="text-[13px] text-black hover:text-white">Shop</a></li>
-            <li><a href="javascript:void(0)" className="text-[13px] text-black hover:text-white">About</a></li>
-            <li><a href="javascript:void(0)" className="text-[13px] text-black hover:text-white">Contact</a></li>
+            <li><a href="#" className="text-[13px] text-black hover:text-blue-800">Home</a></li>
+            <li><a href="#" className="text-[13px] text-black hover:text-blue-800">Shop</a></li>
+            <li><a href="#" className="text-[13px] text-black hover:text-blue-800">Blog</a></li>
+            <li><a href="#" className="text-[13px] text-black hover:text-blue-800">Contact</a></li>
             
           </ul>
         </div>
@@ -31,16 +31,16 @@ export default function Footer() {
         <div className="space-y-3">
           <h6 className="text-sm text-gray-400 font-medium">Help</h6>
           <ul className="space-y-1">
-            <li><a href="javascript:void(0)" className="text-[13px] text-black hover:text-white">Payment Options</a></li>
-            <li><a href="javascript:void(0)" className="text-[13px] text-black hover:text-white">Returns</a></li>
-            <li><a href="javascript:void(0)" className="text-[13px] text-black hover:text-white">Privacy Policy</a></li>
+            <li><a href="javascript:void(0)" className="text-[13px] text-black hover:text-blue-800">Payment Options</a></li>
+            <li><a href="javascript:void(0)" className="text-[13px] text-black hover:text-blue-800">Returns</a></li>
+            <li><a href="javascript:void(0)" className="text-[13px] text-black hover:text-blue-800">Privacy Policy</a></li>
             
           </ul>
         </div>
         <div className="space-y-2.5">
           <h6 className="text-sm text-white font-medium">Newsletter</h6>
           <ul className="space-y-2">
-            <li className="text-[15px] text-black hover:text-yellow-50"><u>Enter Your Email Address</u></li>            
+            <li className="text-[15px] text-black hover:text-blue-800"><u>Enter Your Email Address</u></li>            
             
           </ul>
         </div>
